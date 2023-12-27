4159121bb1a30ca15c0c535c92af61f31ae1d63e
ctrevai <ctrevai@gmail.com>
2023-12-27 22:08:20 +0800

    use api

--- single_prompt.py
+++ single_prompt.py
@@ -1,28 +1,32 @@
-import boto3
+# import boto3
 import json
 import streamlit as st
+import requests
 
-bedrock = boto3.client(service_name='bedrock-runtime')
-modelId = 'anthropic.claude-v2:1'
-accept = 'application/json'
-contentType = 'application/json'
-
+# bedrock = boto3.client(service_name='bedrock-runtime')
+# modelId = 'anthropic.claude-v2:1'
+# accept = 'application/json'
+# contentType = 'application/json'
+prompt = ""
 prompt = st.text_input("Enter your prompt")
 
 if prompt:
 
-    body = json.dumps({
-        "prompt": "\n\nHuman:\n\n" + prompt + "\n\nAssistant:",
-        "max_tokens_to_sample": 300,
-        "temperature": 0.1,
-        "top_p": 0.9,
-    })
+    # body = json.dumps({
+    #     "prompt": "\n\nHuman:\n\n" + prompt + "\n\nAssistant:",
+    #     "max_tokens_to_sample": 300,
+    #     "temperature": 0.1,
+    #     "top_p": 0.9,
+    # })
 
-    response = bedrock.invoke_model(
-        body=body, modelId=modelId, accept=accept, contentType=contentType)
+    # response = bedrock.invoke_model(
+    #     body=body, modelId=modelId, accept=accept, contentType=contentType)
 
-    response_body = json.loads(response.get('body').read())
+    response = requests.get(
+        f'https://ztkynucic0.execute-api.us-east-1.amazonaws.com/dev/bedrockprompt?prompt={prompt}')
+
     # text
     # print(response_body.get('completion'))
-
-    st.write(response_body.get('completion'))
+    response_body = response.json().get('body')
+    completion = json.loads(response_body).get('completion')
+    st.write(completion)
